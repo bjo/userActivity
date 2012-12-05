13ab5925c07880ff2f16c37a73d49102559bb97c
bjo <brian88777@gmail.com>
2012-12-04 17:59:01 -0800

    README

--- temp.bash
+++ temp.bash
@@ -5,6 +5,7 @@ do
 	echo $i
 	cat "#your directory"/secure.access.log.2012$i* | python Mapper.py > data/out$i
 	cat data/out$i | python Reducer.py > data/vector$i
+	cat data/vector$i | python Indexer.py
 	cat index | python a.py > popular
 	python Trender.py popular data/out$i > data/trender$i
 done
